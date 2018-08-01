5940eab29ecb2e6387b809f81ed6bff22184f961
Gordon Schulz <gordon.schulz@gmail.com>
2018-08-01 10:48:51 +0200

    Update scripts

--- arch_bootstrap_kde.sh
+++ arch_bootstrap_kde.sh
@@ -21,14 +21,14 @@ if [ "$(id -u)" != 0 ]; then
 fi
 
 # Set up reflector
-pacman -Syu && pacman -S --needed --noconfirm reflector
+pacman -Sy && pacman -S --needed --noconfirm reflector
 reflector --verbose --latest 8 --sort rate --protocol https \
     --save /etc/pacman.d/mirrorlist
 
 # Essential stuff (terminal)
 pacman -S --needed --noconfirm base-devel sudo ansible openssh gpm \
-    netctl networkmanager zsh lsb-release git git-crypt pass pkgfile \
-    neovim python-neovim python2-neovim wipe tmux
+    netctl networkmanager zsh keychain lsb-release git git-crypt pass pkgfile \
+    neovim python-neovim python2-neovim wipe tmux expect
 
 # Essential stuff (graphical)
 grep vendor_id /proc/cpuinfo | grep -q Intel && IS_INTEL_CPU=1
@@ -37,7 +37,7 @@ declare -a GRAPHIC_PACKAGES
 [[ "${IS_INTEL_CPU}" ]] && \
     GRAPHIC_PACKAGES=("vulkan-intel" "libva-intel-driver" "libva" "libvdpau-va-gl")
 [[ "${HAS_NVIDIA_CARD}" ]] && \
-    GRAPHIC_PACKAGES=("nvidia" "nvidia-utils")
+    GRAPHIC_PACKAGES=("nvidia" "nvidia-utils" "libva-vdpau-driver")
 pacman -S --needed --noconfirm mesa xf86-input-libinput xorg xorg-xinit xterm \
     "${GRAPHIC_PACKAGES[@]}"
 
@@ -55,11 +55,11 @@ if ! [ -f /etc/sudoers.d/${USER} ]; then
 fi
 
 # Get custom Repository key and lsign it
-pacman-key --recv-keys ${GPGKEYID}
+pacman-key --keyserver hkp://eu.pool.sks-keyservers.net --recv-keys ${GPGKEYID}
 pacman-key --lsign-key ${GPGKEYID}
 
 # Enable custom Repository in pacman.conf
-if ! grep -E -q "^[${REPONAME}]" /etc/pacman.conf; then
+if ! grep -E -q "^\\[${REPONAME}\\]" /etc/pacman.conf; then
     cat >> /etc/pacman.conf <<- EOF
 		[${REPONAME}]
 		SigLevel = Required TrustedOnly
@@ -69,11 +69,18 @@ fi
 
 # install packages from custom repository
 pacman -Sy --noconfirm
-pacman -S --noconfirm --needed tomb tomb-kdf steghide
+pacman -S --noconfirm --needed tomb tomb-kdf steghide advanced-ssh-config myrepos
 
 # install plasma
-pacman -S --needed --noconfirm plasma-meta konsole dolphin
+pacman -S --needed --noconfirm plasma-meta konsole dolphin kdegraphics-thumbnailers ffmpegthumbs
 systemctl enable sddm.service
+
+# get some nerd-fonts
+su -l $USER -c "mkdir -p ~/.local/share/fonts"
+su -l $USER -c "(cd ~/.local/share/fonts && mkdir -p nerd-fonts-hack && cd nerd-fonts-hack && curl -fLo 'Hack Regular Nerd Font Complete.ttf' 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf?raw=true')"
+su -l $USER -c "(cd ~/.local/share/fonts && mkdir -p nerd-fonts-hack && cd nerd-fonts-hack && curl -fLo 'Hack Regular Nerd Font Complete Mono.ttf' 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf')"
+su -l $USER -c "fc-cache -fv ~/.local/share/fonts"
+
 
 # Disable netctl and enable essential services
 systemctl disable netctl && \
