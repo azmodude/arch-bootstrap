d8d0e5fb379cb3390f634c6f295d24ff59056d56
Gordon Schulz <gordon.schulz@gmail.com>
2019-09-23 13:39:30 +0200

    Reinstate yay

--- arch_bootstrap_i3.sh
+++ arch_bootstrap_i3.sh
@@ -14,20 +14,18 @@ common_reflector
 common_user
 common_essential
 common_makepkg_conf
-#common_add_yay_user
-#common_install_yay
+common_add_yay_user
+common_install_yay
 common_graphical
 
 # install i3 and needed packages
 pacman -S --needed --noconfirm i3-gaps lightdm lightdm-webkit2-greeter arandr \
     polkit-gnome pavucontrol pulseaudio xss-lock physlock scrot wmctrl gtk2 \
     udiskie dunst rofi compton feh
-pacman -S --needed --noconfirm tomb i3lock-fancy-git i3lock-color-git \
-    otf-font-awesome-4 nordic-theme-git nordic-polar-theme-git yay
 
 # aur
-#common_install_aur tomb i3lock-fancy-git i3lock-color-git
-#common_install_aur otf-font-awesome-4 nordic-theme-git nordic-polar-theme-git
+common_install_aur tomb i3lock-fancy-git i3lock-color-git
+common_install_aur otf-font-awesome-4 nordic-theme-git nordic-polar-theme-git
 
 # bumblebee stuff
 pacman -S --needed --noconfirm xdg-utils xdotool xorg-xprop libnotify \
@@ -36,7 +34,7 @@ pacman -S --needed --noconfirm python-i3ipc
 common_install_aur python-i3ipc
 
 common_lightdm
-#common_remove_yay_user
+common_remove_yay_user
 common_services
 common_networkmanager_iwd
 common_keymap
