3006fce281e050ada54fb803b0426f3881a4d456
Gordon Schulz <gordon.schulz@gmail.com>
2019-10-08 12:21:47 +0200

    Update packages, remove lightdm

--- arch_bootstrap_i3.sh
+++ arch_bootstrap_i3.sh
@@ -19,16 +19,15 @@ common_install_yay
 common_graphical
 
 # install i3 and needed packages
-pacman -S --needed --noconfirm i3-gaps i3lock lightdm lightdm-webkit2-greeter \
-    arandr pavucontrol pulseaudio xss-lock scrot wmctrl gtk3 gtk2 udiskie \
-    dunst rofi compton gnome-themes-extra adwaita-icon-theme polkit-gnome \
-    brightnessctl feh
+pacman -S --needed --noconfirm i3-gaps i3status \
+    arandr pavucontrol pulseaudio xss-lock \
+    scrot wmctrl gtk3 gtk2 udiskie dunst rofi compton gnome-themes-extra \
+    adwaita-icon-theme polkit-gnome brightnessctl feh compton kitty
 
 # aur
 common_install_aur tomb i3lock-fancy-git i3lock-color-git
 common_install_aur otf-font-awesome-4
 
-common_lightdm
 common_remove_yay_user
 common_services
 common_networkmanager_iwd
