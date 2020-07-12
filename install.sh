2e051a7451b8ce15f8cb07c6519ca6da2e7ffc9c
Gordon Schulz <gordon.schulz@gmail.com>
2020-07-12 19:46:50 +0200

    Update test installer

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo INSTALL_DISK=/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi3-0-4 ROOT_PASSWORD=test LUKS_PASSPHRASE=test DISK_LAYOUT=lvmxfs HOSTNAME_FQDN=test.azmo.ninja SWAP_SIZE=16 /vagrant/arch_install.sh
+sudo INSTALL_DISK=/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi3-0-4 ROOT_PASSWORD=test LUKS_PASSPHRASE=test DISK_LAYOUT=btrfs HOSTNAME_FQDN=test.azmo.ninja SWAP_SIZE=2 /vagrant/arch_install.sh
